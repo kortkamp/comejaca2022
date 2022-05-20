171ce443f4c7b1163f6a6f682b4010a9492d9752
kortkamp <marcelusmedius@gmail.com>
2022-05-20 07:57:56 -0300

    feat: send mail when registrarting

--- src/modules/registrations/services/CreateRegistrationService.ts
+++ src/modules/registrations/services/CreateRegistrationService.ts
@@ -1,4 +1,8 @@
+import path from 'path';
 import { inject, injectable } from 'tsyringe';
+
+import { IMailProvider } from '@shared/container/providers/MailProvider/models/IMailProvider';
+import ITemplateProvider from '@shared/container/providers/TemplateProvider/models/ITemplateProvider';
 
 // import ErrorsApp from '@shared/errors/ErrorsApp';
 
@@ -10,12 +14,43 @@ class CreateRegistrationService {
   constructor(
     @inject('RegistrationsRepository')
     private registrationsRepository: IRegistrationsRepository,
+
+    @inject('TemplateProvider')
+    private templateProvider: ITemplateProvider,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
   ) {}
 
   public async execute(data: ICreateRegistrationDTO) {
-    const role = await this.registrationsRepository.create(data);
+    const registration = await this.registrationsRepository.create(data);
 
-    return role;
+    const templateFile = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'confirmation_mail.hbs',
+    );
+
+    const linkUpdate = `${process.env.MAIN_URL}/atualizacao.html?id=${registration.id}`;
+    const linkPrint = `${process.env.MAIN_URL}/api/registrations/print/${registration.id}`;
+    const name = registration.nome_completo;
+
+    const templateHTML = await this.templateProvider.parse({
+      file: templateFile,
+      variables: { linkUpdate, linkPrint, name },
+    });
+
+    const message = {
+      to: registration.email,
+      from: 'confirmacao@comejaca.org.br',
+      subject: 'Confirmação de inscrição 2022',
+      html: templateHTML,
+    };
+
+    await this.mailProvider.sendMail(message);
+
+    return registration;
   }
 }
 
