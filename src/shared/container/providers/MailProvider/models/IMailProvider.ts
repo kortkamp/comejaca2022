7bc81f0840d00d98248ce2dd883481719506808f
kortkamp <marcelusmedius@gmail.com>
2022-05-20 12:49:31 -0300

    refactor: make sendMail async

--- src/shared/container/providers/MailProvider/models/IMailProvider.ts
+++ src/shared/container/providers/MailProvider/models/IMailProvider.ts
@@ -1,4 +1,4 @@
-import { ISendMailDTO } from '../dto/ISendMailDTO';
+import { ISendMailDTO } from '../dtos/ISendMailDTO';
 
 interface IMailProvider {
   sendMail(data: ISendMailDTO): Promise<void>;
